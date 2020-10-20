d6a2afa691d379395b2a19935fcdb60fb325f407
Tomasz (ttr) Rudzki <ttr@ttrsite.net>
2020-10-20 20:26:32 +0100

    Small clenup

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Warm Food
 
+Originall by Deus13, TLD update by ttr.
+
 Makes MRE auto heat up on first open and gives the player a warming up buff. Meat and fish now to give a warming up buff when still warm from cooking.
 Also add a multiplier for the calories of meat and fish. For those of you how dont aggree whit the engergy density of the meat ingame.
 
@@ -9,3 +11,8 @@ Everthing customizable in the ModSettings.
 This is a mod for **The Long Dark** 1.83+ by Hinterland Games Studio, Inc.
 
 For 1.56 version, please see orignall by Deus13 - https://github.com/Deus13/WarmFood
+
+Know issues:
+* calories in cooking window are disaplyed with out vanilla (nofix)
+* changing calories in mod settings will not be reflected on current inventory until food will chnage state (eg cooked)
+* stacktrace when applied warmt buffer - buffer is still applied
--- scr/Implementation.cs
+++ scr/Implementation.cs
@@ -7,12 +7,6 @@ namespace WarmFood
 {
     internal class Implementation : MelonMod
     {
-        private const string NAME = "Warm-Food";
-        //private static bool RabbitSnared = false;
-        //private static float TtnUAH;
-
-        // private static bool AcusticHalluzinationsActive = false;
-
 
         public override void OnApplicationStart() {
             Debug.Log($"[{InfoAttribute.Name}] Version {InfoAttribute.Version} loaded!");
@@ -24,25 +18,23 @@ namespace WarmFood
         {
             if ((bool)gi.m_FoodItem)
             {
-
                 string name = gi.name.ToLower();
-                if (name.Contains("mre")&& Settings.options.MeatHeating)
+                if (name.Contains("mre") && Settings.options.MREheating)
                 {
 
-                   // AccessTools.Field(typeof(FoodItem), "m_PreventHeatLoss").SetValue(gi, true);
+                    // AccessTools.Field(typeof(FoodItem), "m_PreventHeatLoss").SetValue(gi, true);
 
-                    //Log(gi.m_FoodItem.m_CaloriesRemaining.ToString() + "   " + (gi.m_FoodItem.m_CaloriesTotal*(1-nV)).ToString() + "   " + nV);
+                    // MelonLogger.Log(gi.m_FoodItem.m_CaloriesRemaining.ToString() + "   " + (gi.m_FoodItem.m_CaloriesTotal*(1-nV)).ToString() + "   " + nV);
                     if (Mathf.Abs(gi.m_FoodItem.m_CaloriesRemaining - gi.m_FoodItem.m_CaloriesTotal * (1 - nV))<1) //Initial selfheating
                     {
                         if (gi.m_FreezingBuff == null) gi.m_FreezingBuff = new FreezingBuff();
                         gi.m_FreezingBuff.m_InitialPercentDecrease = 10f* Settings.options.MREBuffScale;
                         gi.m_FreezingBuff.m_RateOfIncreaseScale = 0.5f;
-                        gi.m_FreezingBuff.m_DurationHours = 2 * Settings.options.MREBuffDuration;
+                        gi.m_FreezingBuff.m_DurationHours = 2f * Settings.options.MREBuffDuration;
                         gi.m_FoodItem.m_HeatPercent = 100;
                         gi.m_FoodItem.m_PercentHeatLossPerMinuteIndoors = 0.5f;
                         gi.m_FoodItem.m_PercentHeatLossPerMinuteOutdoors = 1f;
 
-                        Debug.Log(gi.m_FreezingBuff.m_InitialPercentDecrease.ToString() + "   " + gi.m_FreezingBuff.m_RateOfIncreaseScale.ToString() + "   " + gi.m_FreezingBuff.m_DurationHours.ToString());
                         gi.m_FreezingBuff.Apply(nV);
                     }
                     if(gi.m_FoodItem.IsHot())
@@ -68,7 +60,7 @@ namespace WarmFood
                 gi.m_FoodItem.m_CaloriesTotal *= Settings.options.Meatkcal;
                 gi.m_FoodItem.m_CaloriesRemaining *= Settings.options.Meatkcal;
 
-                if (!gi.m_FoodItem.m_IsRawMeat&& Settings.options.MeatHeating)
+                if (!gi.m_FoodItem.m_IsRawMeat && Settings.options.MeatHeating)
                 {
                     //if (gi.m_Cookable == null) gi.m_Cookable = new Cookable();
                     //SetCookable(gi,true);
@@ -79,7 +71,7 @@ namespace WarmFood
                     gi.m_FoodItem.m_HeatPercent = 100;
 
                     if (gi.m_FreezingBuff == null) gi.m_FreezingBuff = new FreezingBuff();
-                    gi.m_FreezingBuff.m_InitialPercentDecrease = 10f* Settings.options.MeatScale;
+                    gi.m_FreezingBuff.m_InitialPercentDecrease = 10f * Settings.options.MeatScale;
                     gi.m_FreezingBuff.m_RateOfIncreaseScale = 0.5f;
                     gi.m_FreezingBuff.m_DurationHours = 1f * Settings.options.MeatDuration;
                 }
@@ -87,6 +79,7 @@ namespace WarmFood
             }
             if ((bool)gi.m_FoodItem && gi.m_FoodItem.m_IsFish)
             {
+                
                 gi.m_FoodItem.m_CaloriesTotal *= Settings.options.Fishkcal;
                 gi.m_FoodItem.m_CaloriesRemaining *= Settings.options.Fishkcal;
 
@@ -100,7 +93,7 @@ namespace WarmFood
                     if (gi.m_FreezingBuff == null) gi.m_FreezingBuff = new FreezingBuff();
                     gi.m_FreezingBuff.m_InitialPercentDecrease = 10f * Settings.options.MeatScale;
                     gi.m_FreezingBuff.m_RateOfIncreaseScale = 0.5f;
-                    gi.m_FreezingBuff.m_DurationHours = 1f* Settings.options.MeatDuration;
+                    gi.m_FreezingBuff.m_DurationHours = 1f * Settings.options.MeatDuration;
                 }
             }
 
